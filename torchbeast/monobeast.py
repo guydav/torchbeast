8e7a3973755e66d83c880fabb6ac7575386fe027
Guy Davidson <guydav@gmail.com>
2020-04-02 13:59:54 -0400

    Fixed a bug where polybeast saved stats and monobeast didn't.

--- torchbeast/monobeast.py
+++ torchbeast/monobeast.py
@@ -506,6 +506,7 @@ def train(flags):  # pylint: disable=too-many-branches, too-many-statements
                 "model_state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict(),
                 "scheduler_state_dict": scheduler.state_dict(),
+                "stats": stats,
                 "flags": vars(flags),
             },
             path,
